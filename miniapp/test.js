7ae1836eb71d9beb835bf610ea84a23f4c7719a9
Dron939-sketch <Dron939@yandex.ru>
2026-03-19 13:10:11 +0300

    Update test.js

--- miniapp/test.js
+++ miniapp/test.js
@@ -1,23 +1,9 @@
 // ========== test.js ==========
-// Логика 5 этапов тестирования
+// МИНИМАЛЬНАЯ ВЕРСИЯ
 
 const Test = {
-    currentStage: 1,
-    currentQuestion: 0,
-    answers: {},
-
-    async startStage1() {
-        this.currentStage = 1;
-        this.currentQuestion = 0;
-        
-        console.log('📝 Начинаем этап 1');
-        
-        // Здесь будет загрузка первого вопроса с сервера
-        // Пока показываем заглушку
-        alert('Этап 1 теста будет загружен с сервера');
-        
-        // Временно возвращаемся на экран контекста
-        Context.showCompleteScreen(App.userContext);
+    startStage1() {
+        alert('Этап 1 теста');
     }
 };
 
