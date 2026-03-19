babf98aadd6ef59965076a3774c78c6ccac53718
Dron939-sketch <Dron939@yandex.ru>
2026-03-19 13:23:32 +0300

    Update test.js

--- miniapp/test.js
+++ miniapp/test.js
@@ -1,9 +1,15 @@
 // ========== test.js ==========
-// МИНИМАЛЬНАЯ ВЕРСИЯ
+// Логика 5 этапов тестирования
 
 const Test = {
+    currentStage: 1,
+    currentQuestion: 0,
+    answers: {},
+
     startStage1() {
-        alert('Этап 1 теста');
+        this.currentStage = 1;
+        this.currentQuestion = 0;
+        alert('Этап 1 тестирования будет доступен в следующей версии');
     }
 };
 
